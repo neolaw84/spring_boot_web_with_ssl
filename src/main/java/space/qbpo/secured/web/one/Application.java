7d1639bb57b00ea37891aa058b4d25bf89535942
Edward HHA Law <neolaw@gmail.com>
2019-05-25 20:54:07 +1000

    set up a simple Service that adds two integer. set up a RESTController that uses the Service. set up Tests using MockMVC for the Controller. set up Tests using RestTemplate for the Controller.

---
+++ src/main/java/space/qbpo/secured/web/one/Application.java
@@ -0,0 +1,16 @@
+package space.qbpo.secured.web.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/qbpo/secured/web/one/SwoControllerViaRestTests.java
@@ -0,0 +1,41 @@
+package space.qbpo.secured.web.one;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import space.qbpo.secured.web.one.SwoController.SwoIntegerObject;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SwoControllerViaRestTests {
+	
+	@Autowired RestTemplateBuilder restTemplateBuilder;
+	
+	/**
+	 * This test case requires the followings to be put as VM Arguments:
+	 * 
+	 * -Djavax.net.ssl.trustStore="full/path/to/cacerts" -Djavax.net.ssl.trustStorePassword="password"
+	 * the one in git repo has certificate for 'localhost' with password 'changeit'
+	 */
+	@Test
+	public void addTwoAndFive() {
+		UriComponentsBuilder uriComponentBuilder = UriComponentsBuilder.fromHttpUrl(
+				"https://localhost:8080/integer/add_two/"
+				)
+				.queryParam("a", new Integer(2))
+				.queryParam("b", new Integer(5));
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		String uriString = uriComponentBuilder.toUriString();
+		System.out.println(uriString);
+		SwoIntegerObject result = restTemplate.getForObject(
+			uriString, SwoIntegerObject.class
+		);
+		assert(result.getInteger() == 7);
+	}
+}
